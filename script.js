37fdf164e638f12036659fc7bc763049c79a1d41
tejas <sadadetej.27@gmail.com>
2023-08-09 10:46:30 +0530

    Adding the TMDB API

---
+++ script.js
@@ -0,0 +1,53 @@
+const API_KEY = 'api_key=f576a2191032f923db3b5d0f12ae90f1';
+const BASE_URL = 'https://api.themoviedb.org/3'; 
+const API_URL = BASE_URL+'/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main=document.getElementById('main')
+
+getMovies(API_URL)
+
+function getMovies (url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        showMovies(data.results)
+    })
+}
+
+
+
+function showMovies( data ){
+    main.innerHTML = '';
+    
+     data.forEach(movie => {
+        const {title,poster_path,vote_average,overview} = movie;
+        const movieE1 = document.createElement('div');
+        movieE1.classList.add('movie');
+        movieE1.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+ 
+            <div class="overview">
+                <h3>overview</h3>
+                ${overview}
+                </div>` 
+                
+                main.appendChild(movieE1);
+     })
+}
+
+function getColor ( vote ) {
+    if(vote>=8){
+        return 'green'
+    }
+    else if(vote >=5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
